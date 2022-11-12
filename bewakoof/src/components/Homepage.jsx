a4b952127d7316a0d5fbad215054a479753172fd
shivamsherje <sherjes49@gmail.com>
2022-11-12 09:52:36 +0530

    day4

---
+++ bewakoof/src/components/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../homepage/Footer";
+import Navbar from "../homepage/Navbar";
+import Poster from "../homepage/Poster";
+import Allcard from "../homepage/Allcard";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Poster />
+      <Allcard />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ bewakoof/src/components/Signin.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Navbar from "../homepage/Navbar";
+import Footer from "../homepage/Footer";
+import { Navigate } from "react-router-dom";
+
+const Signin = () => {
+  const [goToHome, SetGotoHome] = React.useState(false);
+
+  if (goToHome) {
+    alert("signin successfull");
+    return <Navigate to="/login" />;
+  }
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <br />
+      <div style={{ display: "flex" }}>
+        <div>
+          <h1 style={{ marginLeft: "9rem" }}>
+            Welcome to the world of Bewakoof!
+          </h1>
+          <img
+            style={{ width: "55rem" }}
+            src="https://images.bewakoof.com/web/group-19-1617704502.png"
+          />
+        </div>
+
+        <div className="form" style={{ height: "800px" }}>
+          <h1>Create Account</h1>
+          <div className="fb">
+            <img src="https://i.stack.imgur.com/b6jfF.png" />
+          </div>
+
+          <form>
+            <h4 style={{ marginLeft: "-14rem" }}>FIRST NAME</h4>
+            <input style={{ width: "310px", height: "30px" }} type="email" />
+            <h4 style={{ marginLeft: "-15rem" }}>LAST NAME</h4>
+            <input style={{ width: "310px", height: "30px" }} type="password" />
+            <h4 style={{ marginLeft: "-17rem" }}>EMAIL</h4>
+            <input style={{ width: "310px", height: "30px" }} type="email" />
+            <h4 style={{ marginLeft: "-15rem" }}>PASSWORD</h4>
+            <input style={{ width: "310px", height: "30px" }} type="password" />
+            <br />
+            <br />
+            <button
+              className="loginbutton"
+              style={{
+                width: "320px",
+                height: "40px",
+                color: "white",
+                backgroundColor: "teal",
+                border: "none",
+                fontFamily: "sans-serif",
+                fontSize: "20px",
+              }}
+              onClick={() => {
+                SetGotoHome(true);
+              }}
+            >
+              REGISTER
+            </button>
+            <p
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "13px",
+                color: "#696966",
+              }}
+            >
+              By creating an account or logging in, you agree with Bewakoof's
+              Terms <br /> and Conditions and Privacy Policy.
+            </p>
+            <a href="signin" style={{ textDecoration: "none" }}>
+              <h3>Already have an account? login here </h3>
+            </a>
+          </form>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default Signin;
